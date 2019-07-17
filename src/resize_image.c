bdf04ec9b481628ca3b0eaa77bdb9e02354a148f
Tomasz Kaliciak <tomasz.kaliciak1@gmail.com>
2019-07-17 22:56:12 +0200

    Implementation of bilinear_resize

--- src/resize_image.c
+++ src/resize_image.c
@@ -32,12 +32,42 @@ image nn_resize(image im, int w, int h)
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    return 0;
+    float d1 = x - ceilf(x);
+    float d2 = floorf(x) - x;
+    float d3 = y - ceil(y);
+    float d4 = floorf(y) - y;
+
+    float v1 = get_pixel(im, ceilf(x), ceilf(y), c);
+    float v2 = get_pixel(im , floorf(x), ceilf(y), c);
+    float v3 = get_pixel(im, ceilf(x), floorf(y), c);
+    float v4 = get_pixel(im , floorf(x), floorf(y), c);
+
+    float q1 = v1*d2 + v2*d1;
+    float q2 = v3*d2 + v4*d1;
+    return q1*d4 + q2*d3;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+    image new_image = make_image(w, h, im.c);
+
+    float width_ratio = ((float)im.w) / w;
+    float height_ratio = ((float)im.h) / h;
+
+    for (int y = 0; y < new_image.h; ++y)
+    {
+        for (int x = 0; x < new_image.w; ++x)
+        {
+            for (int z = 0; z < new_image.c; ++z)
+            {
+                float scaled_x = x * width_ratio + (width_ratio * 0.5 - 0.5);
+                float scaled_y = y * height_ratio + (height_ratio * 0.5 - 0.5);
+                float value = bilinear_interpolate(im, scaled_x, scaled_y, z);
+                set_pixel(new_image, x, y, z, value);
+            }
+        }
+    }
+
+    return new_image;
 }
 
