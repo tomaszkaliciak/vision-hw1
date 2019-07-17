aabd7deba6043df6f03bccbe0eb48f2a836bce2f
Tomasz Kaliciak <tomasz.kaliciak1@gmail.com>
2019-07-17 22:26:46 +0200

    Implementation of nn_resize

--- src/resize_image.c
+++ src/resize_image.c
@@ -10,12 +10,28 @@ image nn_resize(image im, int w, int h)
 {
     image new_image = make_image(w, h, im.c);
 
+    float width_ratio = ((float)im.w) / w;
+    float height_ratio = ((float)im.h) / h;
+
+    for (int y = 0; y < new_image.h; ++y)
+    {
+        for (int x = 0; x < new_image.w; ++x)
+        {
+            for (int z = 0; z < new_image.c; ++z)
+            {
+                float scaled_x = x * width_ratio + (width_ratio * 0.5 - 0.5);
+                float scaled_y = y * height_ratio + (height_ratio * 0.5 - 0.5);
+                float value = nn_interpolate(im, scaled_x, scaled_y, z);
+                set_pixel(new_image, x, y, z, value);
+            }
+        }
+    }
+
     return new_image;
 }
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
     return 0;
 }
 
